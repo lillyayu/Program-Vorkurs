7ec98e466da64c1775bd672d75e95ccd8726e427
Jakob <jakob.hav26@gmail.com>
2025-08-13 15:57:10 +0200

    added for/ex02m, added while/ex01

---
+++ For-Schleifen/ex02.py
@@ -0,0 +1,40 @@
+"""
+Eine der häufigsten Anwendungen von For-Schleifen ist das durchlaufen von
+Listen, zum Beispiel zum sortieren, zum herausfinden der größten Zahl, zum
+Überprüfen der Werte in der Liste und viel weiterem.
+
+Diese Aufgabe beschäftigt sich mit allen möglichen Listenoperationen.
+
+Hinweis: in Python gibt es viele dieser Operatoren schon (max() gibt das max-
+imum einer Liste zurück). Diese dürft ihr hier natürlich nicht verwenden.
+
+a) 'alle_wa:
+"""
+
+
+def alle_wahr(list: list[bool]) -> bool:
+    """
+    Überprüft, ob alle Wahrheitswerte in der Liste wahr sind.
+
+    >>> alle_wahr([True, True])
+    True
+    """
+    pass
+
+
+def maximum(list: list[int]) -> int:
+    """
+    Gibt die Größte Zahl der Liste zurück.
+
+    >>> maximum([-6, 2, 0, 9, 2])
+    9
+    """
+    pass
+
+
+def kürzestes_wort(list: list[str]) -> str:
+    pass
+
+
+def check_age_of_all_guests(list: list[int]) -> bool:
+    pass
