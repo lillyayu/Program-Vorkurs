77b406f861dcb0415fc688d0010ab1f33d5a603b
Lenia <lenia.github@kleine-koenig.org>
2025-08-13 12:13:35 +0200

    print.py

---
+++ print.py
@@ -0,0 +1,10 @@
+####  Schreibe ein Programm, welches die selbe Ausgabe erzeugt, jedoch nur einmal den Buchstaben "X" verwendet
+
+print("XXXX\nXXXX\nXXXX\n")
+
+if __name__ != "__main__":
+
+    # Aufgabe 1
+    print(3 * (4 * "X" + "\n"))
+
+# Aufgabe 2
