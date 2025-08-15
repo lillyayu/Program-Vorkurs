5a8909eb0ff55ad42c140c1c603ef76946086657
Jakob <jakob.hav26@gmail.com>
2025-08-15 19:26:41 +0200

    added modulo.py

---
+++ Zahlen/rechnen.py
@@ -0,0 +1,11 @@
+"""
+In dieser Aufgabe beschäftigen wir uns mit der ursprünglichen Aufgabe von
+Computern: Dem Rechnen.
+
+Dafür könnt ihr die Rechnenaufgaben einfach ausgeben (wie 2+2 unten).
+
+a)
+"""
+
+if __name__ == "__main__":
+    print(2+2)
